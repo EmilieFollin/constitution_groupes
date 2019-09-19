0803aa7e891e619bd19559633df2a4687584c8e8
Jérôme <jerome.ballue@gmail.com>
2019-09-19 10:32:56 +0200

    Intégration du des view dans l'architecture

--- Application/View/accueil.php
+++ Application/View/accueil.php
@@ -6,4 +6,206 @@
  * Time: 14:53
  */
 ?>
-<h2>Page Accueil</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>blablaal</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <script src="script.js"></script>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.9/css/mdb.min.css" rel="stylesheet">
+</head>
+<!-- Editable table -->
+<body id="body">
+<!-- Editable table -->
+<div class="container-fluid" id="container">
+    <div class="row">
+        <div class="col"></div>
+        <div class="col-8">
+            <div>
+                <img id="image" class="rounded mx-auto d-block" src="assets/img/cropped-logo-school-2.png">
+            </div>
+        </div>
+
+        <div class="col"></div>
+
+    </div>
+</div>
+<div class="container" id="container2">
+    <div class="row">
+        <div class="col"></div>
+        <div class="col-10">
+
+            <div class="card">
+                <h3 class="card-header text-center font-weight-bold text-uppercase py-4">Ajout des étudiants dans la base</h3>
+                <div class="card-body">
+                    <div  class="table-editable">
+                        <span class="table-add float-right mb-3 mr-2">
+                            <h4>Nombre de groupe</h4>
+                            <div class="def-number-input number-input safari_only rounded mx-auto d-block">
+                              <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()" class="minus"></button>
+                              <input class="quantity" min="2" max="10" name="quantity" value="2" type="number">
+                              <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()" class="plus"></button>
+                            </div>
+                        </span>
+                        <table id="matable" class="table table-bordered table-responsive-md table-striped text-center">
+                            <thead>
+                            <tr>
+                                <th class="text-center">Nom Prénom</th>
+                                <th class="text-center">Type Personnalité</th>
+                                <th class="text-center"> Moyenne Back</th>
+                                <th class="text-center"> Moyenne Front</th>
+
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+
+                            </tr>
+                            <!-- This is our clonable table line -->
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+
+                            </tr>
+                            <!-- This is our clonable table line -->
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+
+                            </tr>
+                            <!-- This is our clonable table line -->
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                            </tr>
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+
+                            </tr>
+                            <!-- This is our clonable table line -->
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+
+                            </tr>
+                            <!-- This is our clonable table line -->
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+
+                            </tr>
+                            <!-- This is our clonable table line -->
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                            </tr>
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+
+                            </tr>
+                            <!-- This is our clonable table line -->
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+
+                            </tr>
+                            <!-- This is our clonable table line -->
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+
+                            </tr>
+                            <!-- This is our clonable table line -->
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                            </tr>
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+
+                            </tr>
+                            <!-- This is our clonable table line -->
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+
+                            </tr>
+                            <!-- This is our clonable table line -->
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+
+                            </tr>
+                            <!-- This is our clonable table line -->
+                            <tr>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                                <td class="pt-3-half" contenteditable="true"></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <span class="table-add mb-3 mr-2 rounded mx-auto d-block"><a href="affichage.html" class="text-success "><button type="button" class="btn btn-success btn-rounded btn-lg rounded mx-auto d-block">Enregistrer et création des groupes</button></a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+</div>
+
+</body>
+
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.9/js/mdb.min.js"></script>
+</html>
+
+
--- Application/View/groupe.php
+++ Application/View/groupe.php
@@ -7,4 +7,221 @@
  */
 var_dump($student->getId());
 ?>
-<h2>Groupe Fonctionne</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>blablaal</title>
+    <link rel="stylesheet" href="assets/style.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.9/css/mdb.min.css" rel="stylesheet">
+</head>
+
+<body id="body">
+<!-- Editable table -->
+<div class="container-fluid" id="container">
+    <div class="row">
+        <div class="col"><a href="index.html"><button class="btn btn-danger btn-rounded btn-lg">Retour</button></a>  </div>
+        <div class="col-8">
+            <div>
+                <img id="image" class="rounded mx-auto d-block" src="assets/img/cropped-logo-school-2.png">
+            </div>
+        </div>
+
+        <div class="col"></div>
+
+    </div>
+</div>
+<div class="container-fluid" id="container2">
+    <div class="row">
+        <div class="col"></div>
+        <div class="col-8">
+            <div id="jumbo2" class="jumbotron card card-image">
+                <h1 class="text-center"><strong>AFFICHAGE DES GROUPES </strong>  <i class="fas fa-chevron-down fa-1x ml-4"></i></h1>
+                <div class="row">
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col"></div>
+
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div id="affichagegrp" class="col-4 rounded mx-auto d-block"><!-- Card -->
+            <div class="card" id="bkcard">
+
+                <!-- Card image -->
+                <div class="view overlay">
+                    <a href="#!">
+                        <div class="mask rgba-white-slight"></div>
+                    </a>
+                </div>
+
+                <!-- Card content -->
+                <div class="card-body">
+
+                    <!-- Title -->
+                    <h3 class="card-title text-center"><strong>GROUPE 1</strong></h3>
+                    <!-- Text -->
+                    <table class="table table-striped text-center">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col"><strong>Nom</strong></th>
+                            <th scope="col"><strong>Prénom</strong></th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+
+                        </tr>
+                        </tbody>
+                    </table>
+
+                </div>
+
+            </div>
+            <!-- Card --></div>
+        <div id="affichagegrp" class="col-4 rounded mx-auto d-block">
+            <!-- Card -->
+            <div class="card" id="bkcard">
+
+                <!-- Card image -->
+                <div class="view overlay">
+                    <a href="#!">
+                        <div class="mask rgba-white-slight"></div>
+                    </a>
+                </div>
+
+                <!-- Card content -->
+                <div class="card-body">
+
+                    <!-- Title -->
+                    <h3 class="card-title text-center"><strong>GROUPE 2</strong></h3>
+                    <!-- Text -->
+                    <table class="table table-striped text-center">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col"><strong>Nom</strong></th>
+                            <th scope="col"><strong>Prénom</strong></th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+            <!-- Card --></div>
+        <div id="affichagegrp" class="col-4 rounded mx-auto d-block"><!-- Card -->
+            <div class="card" id="bkcard">
+
+                <!-- Card image -->
+                <div class="view overlay">
+                    <a href="#!">
+                        <div class="mask rgba-white-slight"></div>
+                    </a>
+                </div>
+
+                <!-- Card content -->
+                <div class="card-body">
+
+                    <!-- Title -->
+                    <h3 class="card-title text-center"><strong>GROUPE 3</strong></h3>
+                    <!-- Text -->
+                    <table class="table table-striped text-center">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col"><strong>Nom</strong></th>
+                            <th scope="col"><strong>Prénom</strong></th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+
+                        </tr>
+                        </tbody>
+                    </table>
+
+                </div>
+
+            </div>
+            <!-- Card --></div>
+
+    </div>
+
+</div>
+</body>
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.9/js/mdb.min.js"></script>
+</html>
+
+
